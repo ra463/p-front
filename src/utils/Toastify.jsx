4cdab7db9ce14dd831e7824e6f133848c0f25f76
Rachit Patel <80671047+ra463@users.noreply.github.com>
2024-05-27 18:56:22 +0530

    Delete src/utils/Toastify.jsx

--- src/utils/Toastify.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { ToastContainer } from "react-toastify";
-
-export default function Toastify() {
-  return <ToastContainer position="bottom-right"   autoClose={3000} theme="dark" />;
-}
