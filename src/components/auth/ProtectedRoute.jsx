8df60237c2c3d99ddb62a2a7528c3abf1caff063
Rachit Patel <80671047+ra463@users.noreply.github.com>
2024-05-27 18:10:42 +0530

    changes in ProtectedRoute.jsx

--- src/components/auth/ProtectedRoute.jsx
+++ src/components/auth/ProtectedRoute.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoute = ({ children, user, redirect = "/" }) => {
-  if (!user) return <Navigate to={redirect} />;
+  const doThings = () => {
+    alert("Please login first");
+  };
+  if (!user)
+    return (
+      <>
+        <Navigate to={redirect} />
+        {doThings()}
+      </>
+    );
   return children ? children : <Outlet />;
 };
 
